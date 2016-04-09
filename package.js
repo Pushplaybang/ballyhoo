42603718c5f14789393145e4c4d84aa6144779ff
Paul van Zyl <paul@nonacreative.com>
2016-04-10 01:25:52 +0200

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 Package.describe({
   name: 'pushplaybang:ballyhoo',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Bloody simple pub/sub JS events',
   git: 'https://github.com/Pushplaybang/ballyhoo',
   documentation: 'README.md',
